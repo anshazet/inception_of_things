c5074a189a94f129d812b6370d41bd4954bc01c3
NineSuper <NineSuper66@gmail.com>
2025-04-15 12:57:47 +0200

    remove dir bonus/

--- bonus/scripts/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Placeholder script for Bonus part
-# This will integrate GitLab with the CI/CD pipeline
-
-echo "Setting up GitLab integration..."
-# Actual implementation to be added 
