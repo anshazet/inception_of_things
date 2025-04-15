3c95e2695291c5f1d82ddf0f5c1e1fcfb4daa8a4
NineSuper <NineSuper66@gmail.com>
2025-04-15 10:48:10 +0200

    Update script files

--- p1/scripts/install_agent.sh
+++ p1/scripts/install_agent.sh
@@ -2,7 +2,12 @@
 
 # Update system
 apt-get update -y
+apt install -y net-tools
 apt-get upgrade -y
+
+# Détecte l'interface réseau privée
+PRIVATE_IFACE=$(ip -o -4 addr show | awk '/192\.168\.56\./ {print $2; exit}')
+echo "Detected private network interface: $PRIVATE_IFACE"
 
 # Wait for the server token to be available
 while [ ! -f /vagrant/confs/node-token ]; do
@@ -12,20 +17,12 @@ done
 
 echo "Node token found. Proceeding with installation..."
 
-# Get the token
-export K3S_TOKEN=$(cat /vagrant/confs/node-token)
-export K3S_URL=https://192.168.56.110:6443
+# Set variables
+K3S_TOKEN=$(cat /vagrant/confs/node-token)
+K3S_URL="https://192.168.56.110:6443"
+INSTALL_K3S_EXEC="--node-ip=192.168.56.111 --flannel-iface=${PRIVATE_IFACE}"
 
-# Set up K3s agent with proper network interface
-export INSTALL_K3S_EXEC="--node-ip=192.168.56.111 --flannel-iface=eth1"
+# Install K3s agent with vars available to installer
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="$INSTALL_K3S_EXEC" K3S_URL="$K3S_URL" K3S_TOKEN="$K3S_TOKEN" sh -s - agent
 
-# Install K3s agent
-curl -sfL https://get.k3s.io | sh -s - agent
-
-# Export variables for easier access
-echo "export K3S_TOKEN=${K3S_TOKEN}" >> /home/vagrant/.bashrc
-echo "export K3S_URL=${K3S_URL}" >> /home/vagrant/.bashrc
-
-# Wait for k3s to be ready
-sleep 10
-echo "K3s Agent installation complete!" 
+echo "K3s Agent installation complete!"
--- p1/scripts/install_server.sh
+++ p1/scripts/install_server.sh
@@ -2,10 +2,16 @@
 
 # Update system
 apt-get update -y
+apt install -y net-tools
 apt-get upgrade -y
 
+# Détecte l'interface réseau privée
+PRIVATE_IFACE=$(ip -o -4 addr show | awk '/192\.168\.56\./ {print $2; exit}')
+echo "Detected private network interface: $PRIVATE_IFACE"
+
 # Set up K3s server
-export INSTALL_K3S_EXEC="--bind-address=192.168.56.110 --node-ip=192.168.56.110 --flannel-iface=eth1"
+export INSTALL_K3S_EXEC="--bind-address=192.168.56.110 --node-ip=192.168.56.110 --flannel-iface=${PRIVATE_IFACE}"
+# export INSTALL_K3S_EXEC="--bind-address=192.168.56.110 --node-ip=192.168.56.110 --flannel-iface=eth1"
 
 # Install K3s without traefik
 curl -sfL https://get.k3s.io | sh -s - --disable=traefik
@@ -29,4 +35,4 @@ echo "export K3S_URL=https://192.168.56.110:6443" >> /home/vagrant/.bashrc
 
 # Wait for k3s to be ready
 sleep 10
-echo "K3s Server installation complete!" 
+echo "K3s Server installation complete!"
